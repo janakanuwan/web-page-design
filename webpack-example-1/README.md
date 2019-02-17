f23830fe15e9eeea89eed117a9a17b08453abd5b
45715403+janakanuwan <45715403+janakanuwan@users.noreply.github.com>
2019-02-18 01:14:34 +0800

    Added webpack-example-1

---
+++ webpack-example-1/README.md
@@ -0,0 +1,8 @@
+# webpack-example-1
+
+
+To install dependency modules locally, run:
+```
+npm install
+```
+
---
+++ webpack-example-1/src/index.js
@@ -0,0 +1,16 @@
+
+const tutorialInfo = function () {
+	console.log('TutorialInfo');
+
+	const title = document.createElement('h2');
+	title.textContent = 'Tutoial 3';
+	
+	const description = document.createElement('p');
+	description.textContent = 'This tutorial try to explain the basic concepts of JS build tools';
+
+	const container = document.querySelector('#container');
+	container.appendChild(title);
+	container.appendChild(description);
+};
+
+tutorialInfo();
