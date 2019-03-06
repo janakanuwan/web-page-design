20a3b10f7ef79e844d463b79b0a7c03c4a8d9811
45715403+janakanuwan <45715403+janakanuwan@users.noreply.github.com>
2019-03-06 09:10:02 +0800

    Added Hero

---
+++ webpack-example-react-2/src/view/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Hero extends React.Component{
+
+
+    render(){
+
+        return (
+            <div>
+                <h3></h3>
+                <p>"Powers: '</p>
+            </div>
+        );
+    }
+}
+
+export default Hero;
