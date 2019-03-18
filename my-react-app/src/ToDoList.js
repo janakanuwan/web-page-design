8a37043065a9c5ab8e038f017de39cfae3c5abf3
45715403+janakanuwan <45715403+janakanuwan@users.noreply.github.com>
2019-03-19 00:45:03 +0800

    Added todo list

---
+++ my-react-app/src/ToDoList.js
@@ -0,0 +1,85 @@
+import React from 'react';
+
+// Ref: https://codepen.io/marekdano/pen/bVNYpq
+
+class ToDoListItems extends React.Component {
+    render() {
+        const {items} = this.props;
+        const itemList = items.map((item, index) => <li key={index}> {item} </li>)
+
+        return (
+            <div>
+                <h3>ToDo Items</h3>
+                <ul> {itemList} </ul>
+            </div>
+        );
+    }
+}
+
+
+// Ref: https://codepen.io/marekdano/pen/bVNYpq
+class ToDoListForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        // https://reactjs.org/docs/uncontrolled-components.html
+        this.formInput = React.createRef();
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onSubmit(event) {
+        event.preventDefault(); // stop page being reload
+
+        // take the input value from 'ref'
+        const newItemValue = this.formInput.current.value;
+        if (newItemValue) {
+            this.props.addItem(newItemValue);
+            this.formInput.current.value ='';
+        }
+    }
+
+    render() {
+        return (
+            // Ref: https://reactjs.org/docs/forms.html
+            <form ref="form" onSubmit={this.onSubmit} className="form-inline">
+                <input type="text" ref={this.formInput} className="form-control" placeholder="add a new todo..."/>
+                <button type="submit">Add</button>
+            </form>
+        );
+    }
+
+}
+
+class ToDoList extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem(newItem) {
+        const updatedItems = this.state.items.concat(newItem);
+        this.setState({items: updatedItems});
+    }
+
+
+    render() {
+
+        return (
+            <div>
+                <ToDoListItems items={this.state.items} />
+                <ToDoListForm addItem={this.addItem} />
+            </div>
+        );
+
+    }
+}
+
+
+export default ToDoList;
